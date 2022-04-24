904cc96ebc5cfd482b9c1767090bfd2416d27449
unknown <nvhien19@pdu.edu.vn>
2022-04-25 00:06:51 +0700

    add .classpath
    add .project
    add src/Lopdoituong/Vidu1.java

---
+++ src/Lopdoituong/Vidu1.java
@@ -0,0 +1,17 @@
+package Lopdoituong;
+
+public class Vidu1 {
+	
+	int id;
+	String name;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vidu1 s1=new Vidu1();
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+
+	}
+
+}
